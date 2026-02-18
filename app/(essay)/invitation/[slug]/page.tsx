53ec0ec0240f85d816ad85fdd0994ef19cf07814
Karthik Uppu <homanuppu@gmail.com>
2026-02-18 10:19:13 +0530

    .

--- app/(essay)/invitation/[slug]/page.tsx
+++ app/(essay)/invitation/[slug]/page.tsx
@@ -129,7 +129,9 @@ async function InvitationContent({ slug }: { slug: string }) {
       <hr style={hrStyle} />
       <p style={blockquoteStyle}>
         There is no obligation to buy or trade shares of my token. By joining
-        the village, you simply give yourself the option to.
+        the village, you simply give yourself the option to. If you're
+        interested, DM me your onchain address and I'll get your account set up
+        :)
       </p>
     </div>
   );
